db21dadc4240091143a1417deef2c2187cc2907b
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2019-02-02 11:14:42 +0400

    Python for Everybody (Exercise 1)

---
+++ Exercise1.py
@@ -0,0 +1,47 @@
+#Exercise 1: What is the function of the secondary memory in a computer?
+#c) Store information for the long term, even beyond a power cycle
+#Exercise 2: What is a program?
+#A program is a sequence of Python statements that have been crafted to do something
+#Exercise 3: What is the difference between a compiler and an interpreter?
+#interperter:
+#-reads the source code of the program as written by the programmer
+#-parses the source code
+#-interprets the instructions on the fly
+#compiler:
+#-needs to be handed the entire program in a file
+#-it runs a process to translate the high-level source code into machine language
+#-puts the resulting machine language into a file for later execution
+#Exercise 4: Which of the following contains “machine code”?
+#a) The Python interpreter
+#Exercise 5: What is wrong with the following code:
+#>>> primt 'Hello world!' print function is written wrong the line should include Brackets around what should be printed
+#File "<stdin>", line 1
+#primt 'Hello world!'
+#^
+#SyntaxError: invalid syntax
+#>>>
+#THE CORRECT CODE
+#print ("Hello world!")
+#print ('Hello world!')
+#Exercise 6: Where in the computer is a variable such as “x” stored after the following Python line finishes?
+#b) Main Memory
+#Exercise 7: What will the following program print out:
+#x = 43
+#x = x + 1
+#print(x)
+#b) 44
+#Exercise 8: Explain each of the following using an example of a human capability: (1) Central processing unit,
+#(2) Main Memory, (3)Secondary Memory, (4) Input Device, and (5) Output Device. For example,
+# “What is the human equivalent to a Central Processing Unit”?
+#1-the human equivalent to a Central Processing Unit is the brain
+#2-the human equivalent to a Main Memory it's the memory of human that include memories that we can't forget
+#3-the human equivalent to a Secondary Memory it's the memory of human that include memories that we can forget
+#that after some time we forget every thing about it
+#4-the human equivalent to a Input Device are ears, eyes, sensing and nose
+#5-the human equivalent to a Output Device are mouse, all of our natural behaves
+#Exercise 9: How do you fix a “Syntax Error”?
+#syntax error means that you have violated the “grammar” rules of Python
+#sometimes the
+#mistake that needs fixing is actually earlier in the program than where Python
+#noticed it was confused
+#so I should read this line accurately to find the mistake and re-correct it
